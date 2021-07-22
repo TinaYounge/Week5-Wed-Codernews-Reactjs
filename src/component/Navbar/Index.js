959ca8c2a619ad7f5984de18aad4bb378510c0a6
Tina <nguyennga.hcm@gmail.com>
2021-07-23 00:36:26 +0700

    search box

--- src/component/Navbar/Index.js
+++ src/component/Navbar/Index.js
@@ -9,12 +9,33 @@ import {
 } from "react-bootstrap";
 
 const NavbarPage = () => {
-  const [searchInput, setSearchInput] = useState("haha");
+  const [searchInput, setSearchInput] = useState("");
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      let url;
+      if (query) {
+        url = `https://gnews.io/api/v4/search?q=${query}&token=a2c0769df580c64f7b27c4c74f10d0c4`;
+      } else {
+        url = `https://gnews.io/api/v4/top-headlines?token=a2c0769df580c64f7b27c4c74f10d0c4`;
+      }
+      console.log("url", url);
+      const data = await fetch(url);
+      const result = await data.json();
+      console.log("haha", result);
+    };
+    getData();
+  }, [query]);
+
   const handleInput = (event) => {
     event.preventDefault();
     setSearchInput(event.target.value);
   };
 
+  const confirmSearch = () => {
+    setQuery(searchInput);
+  };
   return (
     <>
       <Navbar bg="light" variant="light">
@@ -27,14 +48,18 @@ const NavbarPage = () => {
           </Nav>
           <InputGroup className="mb-3 ">
             <FormControl
-              placeholder="Recipient's username"
+              // placeholder="Recipient's username"
               aria-label="Recipient's username"
               aria-describedby="basic-addon2"
               value={searchInput}
               onChange={handleInput}
             />
-            <Button variant="outline-secondary" id="button-addon2">
-              Button
+            <Button
+              variant="outline-secondary"
+              id="button-addon2"
+              onClick={confirmSearch}
+            >
+              Search
             </Button>
           </InputGroup>
         </Container>
